6b85f0802bcbdfc458fc8ca35d321f2b7dc7a9b3
Lars-m <lam@cphbusiness.dk>
2018-10-31 09:58:37 +0100

    Add the second utility method from our guidelines

--- src/dataFacade.js
+++ src/dataFacade.js
@@ -1,5 +1,21 @@
 const URL = "http://localhost:3456/api";
 
+//The two methods below, are the utility-methods introduced here (use them if you like):
+//https://docs.google.com/document/d/1hF9P65v_AJKCjol_gFkm3oZ1eVTuOKc15V6pcb3iFa8/edit?usp=sharing 
+
+function makeOptions(method, body) {
+  var opts =  {
+    method: method,
+    headers: {
+      "Content-type": "application/json"
+    }
+  }
+  if(body){
+    opts.body = JSON.stringify(body);
+  }
+  return opts;
+ }
+
 function handleHttpErrors(res){
   if(!res.ok){
     return Promise.reject({status: res.status, fullError: res.json() })
